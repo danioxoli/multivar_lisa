020a535cf280a00702ceebbf97c92eba280dfbe3
DOxoli <danioxoli@users.noreply.github.com>
2017-11-17 11:06:20 +1100

    Update multivar_c.py

--- multivar_c.py
+++ multivar_c.py
@@ -5,8 +5,7 @@ Created on Wed Nov 08 15:44:19 2017
 # Project:  Local Multivariate Geary’s c (Anselin, 2017)
 # Purpose:  Application of the Local Geary c statistic to a multivariate context using vector layers 
 # Author:   Daniele Oxoli (daniele.oxoli@polimi.it)
-# Affiliation: a) Department of Civil and Environmental Engineering | GEOlab, Politecnico di Milano, P.zza Leonardo da Vinci 32, 20133, Milano, Italy
-#              b) Department of Infrastructure Engineering, Centre for SDIs and Land Administration, The University of Melbourne, Melbourne, Victoria 3010, Australia
+# Affiliation: Department of Civil and Environmental Engineering | GEOlab, Politecnico di Milano, P.zza Leonardo da Vinci 32, 20133, Milano, Italy
 #
 """
 
@@ -19,17 +18,35 @@ import geopandas as gpd
 import numpy as np
 import scipy.stats as st
 
+#plotting libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 '''
 #INPUT DATA 
 '''
-path = "a polygons.shp"
+#path = "C:/Users/utente/Desktop/multivar_SA/simple_test_c/grid5_clust.shp"
+#
+#df = gpd.read_file(path)
+
+path = "C:/Users/utente/Desktop/multivar_SA/data_au/vmp_irsd_oecd_sa2_melb.shp"
 
 df = gpd.read_file(path)
 
-df.plot(column='k1', cmap='OrRd', scheme='quantiles', edgecolor='black')
-df.plot(column='k2', cmap='OrRd', scheme='quantiles', edgecolor='black')
-df.plot(column='k3', cmap='OrRd', scheme='quantiles', edgecolor='black')
+df.rename(columns={"int_v_avg": "k1", "irsd_f_med": "k2", "oecd_unemp": "k3"},inplace=True)
+
+#simple plot
+#df.plot(column='k1', cmap='OrRd', scheme='quantiles', k=4, edgecolor='black', figsize=(7,7), legend= True)
+
+
+fig, axes = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(21,7))
+df.plot(column='k1', cmap='OrRd', scheme='quantiles', k=4, edgecolor='black', legend= True , ax=axes[0])
+df.plot(column='k2', cmap='OrRd', scheme='quantiles', k=4, edgecolor='black', legend= True, ax=axes[1])
+df.plot(column='k3', cmap='OrRd', scheme='quantiles', k=4, edgecolor='black', legend= True, ax=axes[2])
+fig.suptitle('INDICATOR QUANTILES MAPS', fontsize=16)
+axes[0].set_title("VAMPIRE", fontstyle='italic')
+axes[1].set_title("IRSD", fontstyle='italic')
+axes[2].set_title("OECD", fontstyle='italic')
 
 
 '''
@@ -38,20 +55,19 @@ df.plot(column='k3', cmap='OrRd', scheme='quantiles', edgecolor='black')
 
 weigth_type = 'r' # 'o' = original binary, 'r' = row-stand.
 
-permutations = 19 # number of random permutations
+permutations = 999 # number of random permutations
 
 
 '''
 #SPATIAL WEIGHTS AND ATTRIBUTE MATRICES EXTRACTION
 '''
 
+
 w = ps.weights.Queen.from_dataframe(df)
 w.transform= weigth_type
 wf = w.full()[0]
-
-# list of the column containing the analysis attributes - test with 3
-df.rename(columns={"attribute1": "k1", "attribute2": "k2", "attribute1": "k3"},inplace=True)
-att_arrs = [df['k1'],df['k2'],df['k3']] 
+		
+att_arrs = [df['k1'],df['k2'],df['k3']]
 att_mtx = np.array(att_arrs).transpose()
 
 att_arrs_norm = [(df['k1']-df['k1'].mean())/df['k1'].std(),
@@ -59,10 +75,13 @@ att_arrs_norm = [(df['k1']-df['k1'].mean())/df['k1'].std(),
                  (df['k3']-df['k3'].mean())/df['k3'].std()]
 att_mtx_norm = np.array(att_arrs_norm).transpose()
 
-norm_sum = np.zeros(np.shape(att_mtx_norm)[0])
-for j in range (0, len(norm_sum)):
-    norm_sum[j] = sum(att_mtx_norm[j])
-    
+# normalized attribute sum 
+#norm_sum = np.zeros(np.shape(att_mtx_norm)[0])
+#for j in range (0, len(norm_sum)):
+#    norm_sum[j] = sum(att_mtx_norm[j])
+#    
+#df['norm_sum'] = norm_sum
+
 '''
 #REAL STATISTIC COMPUTATION
 '''
@@ -94,7 +113,7 @@ p_norm = st.norm.sf(abs(C_ki_z_norm))*2 #twosided
 # INFERENCE UNDER RANDOMIZATION ASSUMPTION (CONDITIONAL PERMUTATION)
 '''
 
-#sumulated statistics values
+#simulated statistics 
 
 np.random.seed(12345)
 
@@ -131,49 +150,35 @@ C_ki_z_sim = (C_ki - E_C_ki_perm)/S_C_ki_perm
 
 p_z_sim = st.norm.sf(abs(C_ki_z_sim))*2 #twosided
 
-# simulated p values based on permutations (one-sided), null: spatial randomness
+# simulated p-values based on permutations (one-sided), null: spatial randomness
 
 p_sim = np.zeros((np.shape(att_arrs_norm)[1]))
 
 for i in range(0,np.shape(att_arrs_norm)[1]):
-    above = C_ki_perm[i] >= C_ki[i] 
+    above = C_ki_perm[i] > C_ki[i] 
     larger = above.sum(0)
+    if (permutations - larger) < larger:
+        larger = permutations - larger
     p_sim[i] = ((larger + 1.0) / (permutations + 1.0))
-
- 
+    
+    
 '''
-# INTERPRETATION AND PLOT OF INTERESTING LOCATIONS
-'''
+# ADD TO THE ATTRIBUTE TABLE THE COMPUTED STATISTICS
+''' 
+df['C_ki'] = C_ki    
+df['p_sim'] = p_sim
+df['z_sim'] = C_ki_z_sim
+df['p_norm'] = p_norm
+df['z_norm'] = C_ki_z_norm
 
-# plot reference distribution from permutation for the i_th location  
+# define locations of interest in the dataset and add flags 
 
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#
-#i = 81
-#
-#sns.kdeplot(C_ki, shade=True, color='g', label='perm. mean')
-#plt.vlines(C_ki, 0, 0.005, 'r')
-#plt.vlines(np.mean(C_ki), 0, 10, 'g')
-#plt.vlines(np.mean(C_ki)-np.std(C_ki), 0, 10, 'g','dotted')
-#plt.vlines(np.mean(C_ki)+np.std(C_ki), 0, 10, 'g','dotted')
-##plt.xlim([0.0, 20.0])
-#
-#plt.vlines(C_ki[i], 0, 10, 'r', label='obs statistic')
-#
-#sns.kdeplot(C_ki_perm[i], shade=True, color="c", label='perm. dist.')
-#plt.vlines(C_ki_perm[i], 0, 0.005, 'k')
-#plt.vlines(np.mean(C_ki_perm[i]), 0, 10, 'b')
-#plt.vlines(np.mean(C_ki_perm[i])-np.std(C_ki_perm[i]), 0, 10, 'b', 'dashed')
-#plt.vlines(np.mean(C_ki_perm[i])+np.std(C_ki_perm[i]), 0, 10, 'b', 'dashed')
+sig = p_sim <= 0.05
 
+corr_lower =  C_ki >= np.mean(C_ki)
 
-# plot locations of interest in the dataset 
+corr_higher = C_ki <np.mean(C_ki)
 
-sig = p_sim <= 0.1
-
-corr_lower = C_ki >= np.mean(C_ki)
-corr_higher = C_ki < np.mean(C_ki)
 
 locations = np.zeros((np.shape(att_arrs_norm)[1]))
 
@@ -182,12 +187,110 @@ locations[sig*corr_lower] = -1
 
 df['sig_loc'] = locations
 
-# in the new filed 'sig_loc' 0 depicts not significant locations, -1 significant location with unfit attribute intensities and, 
-# 1 significant location with similar attribute intensities
+
+
+'''-------- TEST UNIVARIATE LOCAL MORAN'S I '''
+
+y1 = df['k1']
+lm1 = ps.Moran_Local(y1, w, transformation = weigth_type, permutations = permutations)
+
+sig = lm1.p_sim <= 0.05
+locations = np.zeros((np.shape(att_arrs_norm)[1]))
+locations[lm1.q==1 * sig] = 1
+locations[lm1.q==3 * sig] = -1
+
+df['lm1'] = locations
+
+#--------------
+
+y2 = df['k2']
+lm2 = ps.Moran_Local(y2, w, transformation = weigth_type, permutations = permutations)
+
+
+sig = lm2.p_sim <= 0.05
+locations = np.zeros((np.shape(att_arrs_norm)[1]))
+locations[lm2.q==1 * sig] = 1
+locations[lm2.q==3 * sig] = -1
+
+df['lm2'] = locations
+
+#--------------
+
+y3 = df['k3']
+lm3 = ps.Moran_Local(y3, w, transformation = weigth_type, permutations = permutations)
+
+
+sig = lm3.p_sim <= 0.05
+locations = np.zeros((np.shape(att_arrs_norm)[1]))
+locations[lm3.q==1 * sig] = 1
+locations[lm3.q==3 * sig] = -1
+
+df['lm3'] = locations
 
 '''-------- SAVE THE MODIFIED GEODATAFRAME TO A NEW SHAPEFILE '''
 
-df.to_file(driver = 'ESRI Shapefile', filename= "../result.shp")
+df.to_file(driver = 'ESRI Shapefile', filename= "C:/Users/utente/Desktop/multivar_SA/simple_test_c/result.shp")
+
+
+'''
+# INTERPRETATION AND PLOT OF INTERESTING LOCATIONS
+'''
+
+# plot reference distribution from permutations for the i_th location  
+
+
+i = 98 #location ID
+fig, ax = plt.subplots(1, figsize=(10,7))
+sns.kdeplot(C_ki, shade=True, color='g', label='obs. dist. with mean +- std')
+#plt.vlines(C_ki, 0, 0.005, 'r')
+plt.vlines(np.mean(C_ki), 0, 10, 'g')
+plt.vlines(np.mean(C_ki)-np.std(C_ki), 0, 10, 'g','dotted')
+plt.vlines(np.mean(C_ki)+np.std(C_ki), 0, 10, 'g','dotted')
+#plt.xlim([0.0, 20.0])
+
+plt.vlines(C_ki[i], 0, 10, 'r', label='obs statistic')
+
+sns.kdeplot(C_ki_perm[i], shade=True, color="c", label='perm. dist. with mean +- std')
+plt.vlines(C_ki_perm[i], 0, 0.005, 'k', label='perm')
+plt.vlines(np.mean(C_ki_perm[i]), 0, 10, 'c')
+plt.vlines(np.mean(C_ki_perm[i])-np.std(C_ki_perm[i]), 0, 10, 'c', 'dashed')
+plt.vlines(np.mean(C_ki_perm[i])+np.std(C_ki_perm[i]), 0, 10, 'c', 'dashed')
+
+# plot univar cluster, aggrgate sum and multivar significant locations
+
+fig, axes = plt.subplots(nrows=1, ncols=3, sharey=True, figsize=(21,7))
+df.plot(column='lm1', cmap = 'bwr', edgecolor='black', legend= True, categorical=True, ax=axes[0])
+df.plot(column='lm2', cmap='bwr', edgecolor='black', legend= True, categorical=True, ax=axes[1])
+df.plot(column='lm3', cmap='bwr', edgecolor='black', legend= True, categorical=True, ax=axes[2])
+fig.suptitle("LOCAL MORAN'I MAPS", fontsize=16)
+axes[0].set_title("VAMPIRE", fontstyle='italic')
+axes[1].set_title("IRSD", fontstyle='italic')
+axes[2].set_title("OECD", fontstyle='italic')
+
+
+fig, ax = plt.subplots(1, figsize=(10,10))
+ax = df.plot(column='sig_locations', cmap='RdYlBu_r', edgecolor='black', legend= True, categorical=True, axes=ax)
+fig.suptitle("MULTIVARIATE SPATIAL ASSOCIATION - GEARY'S C",fontsize=16)
+ax.set_title("VAMPIRE - IRSD - OECD" , fontstyle='italic')
+plt.show()
+
+print ('not significant locations = ' +str(sum(df['sig_locations'] == 0)))
+print ('significant locations of high values = ' + str(sum(df['sig_locations'] == 1)))
+print ('significant locations of low values= ' + str(sum(df['sig_locations'] == -1)))
+
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(14,7))
+df.plot(column='z_norm', cmap='OrRd_r', scheme='quantiles', k=5, edgecolor='black', legend= True,  ax=axes[0])
+df.plot(column='p_sim', cmap='Greys_r', scheme='quantiles', k=5, edgecolor='black', legend= True,  ax=axes[1])
+fig.suptitle("Quntiles  maps - Computed statistics", fontsize=16)
+axes[0].set_title("Computed MULTIVARIATE GEARY'S C z-norm ", fontstyle='italic')
+axes[1].set_title("Pseudo p-values", fontstyle='italic')
+plt.show()
+
+
+#simple plot
+#df.plot(column='sig_locations', cmap='RdYlBu_r', edgecolor='black', legend= True, categorical=True,figsize=(10,10))
+
 
 
 
